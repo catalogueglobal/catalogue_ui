f0b1b697ace3e0475d6193f2d0db5d3b371288b1
Zakaria BOUZIANE <zbouziane@gmail.com>
2016-11-04 09:38:39 +0100

    Using Catalogue core dev env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  rootApi: "http://data.catalogue.global:4000", //"http://178.33.231.17:4001", //rootApi: "https://gtfs-manager-dev.conveyal.com",
+  rootApi: "http://178.33.231.17:4001",
   institutionalUrl: "http://www.catalogue.global",
   authId: "URvcRYyeNMGRFJLRQW9Kp62DHAv7YGBH",
   authDomain: "cataloguetest.eu.auth0.com"
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  rootApi: "http://data.catalogue.global:4000", //"http://178.33.231.17:4001",
+  rootApi: "http://data.catalogue.global:4000",
   institutionalUrl: "http://www.catalogue.global",
   authId: "URvcRYyeNMGRFJLRQW9Kp62DHAv7YGBH",
   authDomain: "cataloguetest.eu.auth0.com"
