1848b020b65295d23f6312c8c89940448cd64b79
cgalant <christophe.galant@gmail.com>
2017-08-03 10:12:59 +0200

    [CONFIG] add a proxy for local env to route API requests to the server (avoid CORS issue)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: false,
-    rootApi: "http://localhost:4000",
+    rootApi: "http://localhost:4200",
     institutionalUrl: "http://www.catalogue.global",
     authId: "URvcRYyeNMGRFJLRQW9Kp62DHAv7YGBH",
     authDomain: "cataloguetest.eu.auth0.com",
